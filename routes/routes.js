151b4d6357b9ec01c2555891d52db37bbbb6d495
Jonas Erixon <je222zj@student.lnu.se>
2017-11-09 11:25:58 +0100

    can present title + image

--- routes/routes.js
+++ routes/routes.js
@@ -12,14 +12,26 @@ router.route('/')
 
             xmlparser(body, (err, result) => {
                 let articles = [];
-                let articles = result.rss.channel[0].item;
-                let title = articles[0].title;
-                let img = articles[5].description;
 
-                let src = img[0].substring(10);
-                src = src.substring(0, src.length-5);
+                let articlesFromAftonbladet = result.rss.channel[0].item;
 
-                res.render('home', {articles: articles, src: src});
+                for (let i = 0; i < articlesFromAftonbladet.length; i++) {
+                    let imgSrc = articlesFromAftonbladet[i].description[0].substring(10);
+                    imgSrc = imgSrc.substring(0, imgSrc.length-5);
+
+                    articles.push({
+                        title: articlesFromAftonbladet[i].title,
+                        imgSrc: imgSrc
+                    });
+                }
+                // let articles = result.rss.channel[0].item;
+                // let title = articles[0].title;
+                // let img = articles[5].description;
+
+                // let src = img[0].substring(10);
+                // src = src.substring(0, src.length-5);
+
+                res.render('home', {articles: articles});
             });            
         });
 

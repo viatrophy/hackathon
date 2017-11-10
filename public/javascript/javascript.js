325dba65cd88dcaf8d102395640c06bbb06e319e
Simon Österdahl <simon2@live.se>
2017-11-10 13:24:28 +0100

    najs

--- public/javascript/javascript.js
+++ public/javascript/javascript.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
     msg.voice = voices[10]; // Note: some voices don't support altering params
     msg.voiceURI = 'native';
     msg.volume = 1; // 0 to 1
-    msg.rate = 10; // 0.1 to 10
+    msg.rate = 1; // 0.1 to 10
     msg.pitch = 1; //0 to 2
     msg.text = allTexts;
     msg.lang = 'sv-SE';
